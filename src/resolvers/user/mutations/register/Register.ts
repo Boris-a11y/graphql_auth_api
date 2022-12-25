548224a94e41e751bd91688d1c45a9630a390195
Boris-a11y <68324526+Boris-a11y@users.noreply.github.com>
2022-12-25 19:05:48 +0100

    Update Register.ts

--- src/resolvers/user/mutations/register/Register.ts
+++ src/resolvers/user/mutations/register/Register.ts
@@ -1,6 +1,4 @@
 import { User } from "../../../../entity/User";
-import { confirmationUrl } from "../../../../utils/confirmationUrl";
-import { sendEmail } from "../../../../utils/sendEmail";
 import { Resolver, Mutation, Arg } from "type-graphql";
 import { registerInput } from "../../inputs/registerInput";
 import * as argon2 from "argon2";
@@ -16,8 +14,6 @@ export class Register {
     });
 
     const user = User.create({ email, password: hashedPassword, age }).save();
-
-    await sendEmail(email, await confirmationUrl((await user).id));
 
     return user;
   }
